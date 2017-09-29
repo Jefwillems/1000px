4b4af7f6383e051e93c8dd1c2289eec366fc6fca
Jefwillems <jefwillems100@gmail.com>
2017-09-29 21:04:11 +0200

    user signup can now be handled in back-end

--- backend/app/routes/users.js
+++ backend/app/routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = express.Router();
 const User = require('../models/user'); // get our mongoose model
+const passport = require('passport');
+const dbconfig = require('../../config/database');
+const jwt = require('jsonwebtoken');
 
 // <link>/api/users/
 routes.get('/', (req, res) => {
@@ -8,11 +11,46 @@ routes.get('/', (req, res) => {
         message: 'Welcome to the coolest users API in the world!'
     });
 });
+
 //route to return all users at <link>/api/users/all
 routes.get('/all', (req, res) => {
     User.find({}, function (err, users) {
         res.json(users);
     });
 });
+
+//route to sign up a user at <link>/api/users/signup
+routes.post('/signup', (req, res) => {
+    if (!req.body.username || !req.body.password || !req.body.email) {
+        //TODO specify what information was missing.
+        res.json({
+            success: false,
+            msg: 'Please fill in all information.'
+        });
+    } else {
+        let isAdmin = false;
+        if (req.body.admin) {
+            isAdmin = req.body.admin;
+        }
+        const newUser = new User({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            admin: isAdmin
+        });
+        newUser.save(function (err) {
+            if (err) {
+                return res.json({
+                    success: false,
+                    msg: JSON.stringify(err)
+                });
+            }
+            res.json({
+                success: true,
+                msg: 'Successful created new user.'
+            });
+        });
+    }
+})
 
 module.exports = routes;
