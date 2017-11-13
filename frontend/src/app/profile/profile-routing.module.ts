dc0c248e3cb5e33fdedf2c585ca7ffa4227686cb
Jefwillems <jefwillems100@gmail.com>
2017-11-13 09:29:29 +0100

    moved login

--- frontend/src/app/profile/profile-routing.module.ts
+++ frontend/src/app/profile/profile-routing.module.ts
@@ -2,9 +2,13 @@ import { AuthGuard } from './../shared/guards/auth.guard';
 import { SettingsComponent } from './settings/settings.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
     { path: 'account', component: SettingsComponent, canActivate: [AuthGuard] }
 ];
 
--- frontend/src/app/profile/profile.module.ts
+++ frontend/src/app/profile/profile.module.ts
@@ -3,13 +3,17 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings/settings.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    ReactiveFormsModule
   ],
-  declarations: [SettingsComponent],
+  declarations: [SettingsComponent, LoginComponent, SignupComponent],
   providers: [AuthGuard]
 })
 export class ProfileModule { }
